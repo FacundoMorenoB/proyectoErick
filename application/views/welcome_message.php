b789190d5a6466c23ac34efdf09496c80dee15a4
facundoarmando.moreno.contractor <facundoarmando.moreno.contractor@bbva.com>
2018-12-14 20:02:47 -0600

    login-hinojosa

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,88 +1,164 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+?>
+<?php $this->load->helper('url'); ?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+ 	<script src="<?=base_url()?>js/jquery-2.1.4.js" type="text/javascript"></script>
+	<link href="<?=base_url()?>css/bootstrap.min.css" rel="stylesheet"/>
+	<link href="<?=base_url()?>css/bootstrap-theme.css" rel="stylesheet"/>
+	<link href="<?=base_url()?>css/normalize.css" rel="stylesheet">
+	<link href="<?=base_url()?>css/estilos.css" rel="stylesheet">
+	<link href="<?=base_url()?>css/paraiconos.css" rel="stylesheet" />
+	<link href="<?=base_url()?>fonts/OleoScript-Regular.ttf" rel="stylesheet" />
+	<link rel="stylesheet" href="<?=base_url()?>css/paraicono.css">
+	<link rel="stylesheet" href="<?=base_url()?>css/sweetalert.css"/>
+	<script src="<?=base_url()?>js/sweetalert-dev.js"></script>
 	<title>Welcome to CodeIgniter</title>
 
-	<style type="text/css">
+	<style>
+@font-face{
+font-family:Fuente;
+src:url(<?=base_url()?>fonts/OleoScript-Regular.ttf);
+}
+    body{
+        background-image: url(<?=base_url()?>/imagenes/puente.jpg);
+        background-size:cover;
+        background-repeat: no-repeat;
+        background-attachment:fixed;
+    }
+    .formulario{
+        transition: 2s;
+        margin-top: 100px;
+        width: 30%;
+        box-shadow: 0px 0px 40px blue,0px 0px 80px white; 
+        
+    }
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+    .formulario:hover{
+        transition: .8s;
+        background-color: rgba(0,0,0,.5);
+    }
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
+
+.logo{
+        height: 75px;
+        margin-top: 40px;
+    }
+    
+ .espaciado{
+        margin-top: 40px;
+    }
+
+    fieldset{
+        transition: 2s;
+        margin-bottom: 50px;
+        border-color: rgba(213,0,0 ,1);
+        border-style: groove;
+        border-width: 5px;
+        border-radius: 20px;
+    }
+    
+
+
+   h3,h4{
+        
+        color:white;
+        text-align: center;
+        font-family: fuente;
+    }
+  
+    .Input{
+        transition: .8s;
+        background-color: rgba(0,0,0,.5);
+        color: white;
+        border-color:#006;
+	border-bottom-color:white;
+        border-bottom-style:groove;
+	border-left:none;
+	border-right:none;
+	border-top:none;
+        border-width: 4px;
+       
+    }
+
+    .Input:hover{
+        transition: .8s;
+	background-color:rgba(55,71,79 ,.5);
+	box-shadow:inset;
+        border-bottom-color:red;
 	}
+    
+    .Input:focus{
+        transition: .8s;
+	border-bottom-color:red;
+    }
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
 
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
+    @media screen and (max-width:750px) {
+        
+        .logo{
+            height: 50px;
 
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+        }
+        .formulario{
+            transition: 2s;
+            width: 95%;
+            margin-top: 10px;
+        }
+    }
+</style>
 </head>
 <body>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+<div class="container formulario">
+         <div class="row">
+               <div class="col-xs-4 col-xs-offset-4  ">
+                <img src="<?=base_url()?>/imagenes/londres%202.jpg" class="logo center-block">
+                </div>
+            
+        </div>
+         <div class=" espaciado">
+                
+                </div>
+        <div class="row">
+            <fieldset class="col-xs-10 col-xs-offset-1">
+            
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+                <legend class="hidden-xs">
+                    <h3>inicio de sesi&oacute;n</h3>
+                </legend>
+                
+                <form role="form" class="form-horizontal">
+                    
+                    <div class="form-group">
+                    <label class="col-xs-12" for="usuario"><h4>Usuario</h4></label>
+                 <div class="col-xs-10 col-xs-offset-1">
+                    <input type="text" id="usuario" class="form-control Input">
+                        
+                        </div>
+                    
+                    </div>
+                    <div class="form-group">
+                    <label class="col-xs-12" for="password"><h4>Password</h4></label>
+                 <div class="col-xs-10 col-xs-offset-1">
+                    <input type="password" id="password" class="form-control col-xs-12 Input">
+                        </div>
+                    
+                    </div>
+                    
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-danger center-block">Aceptar</button>
+                    
+                    </div>
+                 
+                </form>
+            </fieldset>
+        
+        </div>
 </div>
 
 </body>
