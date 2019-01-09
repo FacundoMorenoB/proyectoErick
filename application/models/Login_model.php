ac168233f33a9195ea255e3402f3d7ff4c04a898
Facundo Moreno <facundo_yotuel@yahoo.com.mx>
2019-01-08 23:52:33 -0600

    modelo de roles

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -13,8 +13,8 @@ class Login_model extends CI_Model {
 	public function login($usuario,$password){
 		$this->db->where('codigo_Usuario',$usuario);
 		$this->db->where('password_Usuario',$password);
-		$q = $this->db->get('usuario');
-		if ($q->num_rows()>0) {
+		$query = $this->db->get('usuario');
+		if ($query->num_rows()>0) {
 			return true;
 		}
 		else {
@@ -23,9 +23,19 @@ class Login_model extends CI_Model {
 	}
 
 	public function get_roll($usuario){
-		$this->db->where('codigo_Usuario',$usuario);
-		$r = $this->db->query('select Rol_id_Rol from usuario');
-		echo $r;
+		$this->db->select('pagina.nombre_pagina')
+		->from('usuario')
+		->where('codigo_Usuario',$usuario)
+		->join('pagina_has_rol','usuario.Rol_id_Rol = pagina_has_rol.Rol_id_Rol')
+		->join('pagina','pagina_has_rol.pagina_id_pagina = pagina.id_Pagina');
+		$query = $this->db->get();
+		return $query;
+		/*
+		foreach ($query->result_array() as $row)
+		{
+		        echo $row['nombre_pagina'];
+		}
+		die();*/
 	}
 
 }
